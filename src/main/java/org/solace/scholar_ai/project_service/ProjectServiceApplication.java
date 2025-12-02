4e429ce3fc3e4ca292236b223e9a181302fc6418
Kausar Parvej <kausarparvej3@gmail.com>
2025-12-02 16:14:10 +0600

    Update main application class with new component configurations

--- src/main/java/org/solace/scholar_ai/project_service/ProjectServiceApplication.java
+++ src/main/java/org/solace/scholar_ai/project_service/ProjectServiceApplication.java
@@ -2,13 +2,10 @@ package org.solace.scholar_ai.project_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
+@EnableJpaAuditing
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
